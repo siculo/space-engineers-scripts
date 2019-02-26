eaededb6f0588d7a42908d6a1037c950e78e88e7
siculo <fdgfree@gmail.com>
2019-02-26 16:40:10 +0100

    switchable blocks now are ordered

--- ConsoleApp1/Programs/ControlPanelProgram.cs
+++ ConsoleApp1/Programs/ControlPanelProgram.cs
@@ -16,24 +16,31 @@ namespace IngameScript.ControlPanelProgram
         Echo("c'è un problema: " + result.Success + " " + result.Error);
       } else {
         display = GridTerminalSystem.GetBlockWithName(_ini.Get("config", "monitor").ToString()) as IMyTextPanel;
-        panel = new ControlPanel(33, 12);
-        Menu menu = new Menu("Main");
-
-        System.Collections.Generic.List<IMyFunctionalBlock> blocks = new System.Collections.Generic.List<IMyFunctionalBlock>();
-        GridTerminalSystem.GetBlocksOfType<IMyFunctionalBlock>(blocks, b => MyIni.HasSection(b.CustomData, "switchable"));
-        foreach(IMyFunctionalBlock b in blocks) {
-          MyIniParseResult br;
-          if (_ini.TryParse(b.CustomData, out br) && _ini.Get("switchable", "spinning").ToBoolean()) {
-            menu.AddItem(new BlockSwitcher(b, _bar));
-          } else {
-            menu.AddItem(new BlockSwitcher(b));
-          }
-        }
-        // menu.AddItem(new BlockSwitcher(GridTerminalSystem.GetBlockWithName("Batteria_1") as IMyFunctionalBlock, _bar));
-        panel.SetMenu(menu);
+        int rows = _ini.Get("config", "rows").ToInt32(20);
+        int columns = _ini.Get("config", "columns").ToInt32(15);
+        panel = new ControlPanel(columns, rows);
+        panel.SetMenu(SwitchMenu());
         display.WritePublicText(panel.ToString());
         Runtime.UpdateFrequency = UpdateFrequency.Update100;
       }
+    }
+
+    private Menu SwitchMenu() {
+      Menu menu = new Menu("Switch");
+      System.Collections.Generic.List<IMyFunctionalBlock> blocks = new System.Collections.Generic.List<IMyFunctionalBlock>();
+      GridTerminalSystem.GetBlocksOfType<IMyFunctionalBlock>(blocks, b => MyIni.HasSection(b.CustomData, "switchable"));
+      blocks.Sort(delegate (IMyFunctionalBlock b1, IMyFunctionalBlock b2) {
+        return b1.CustomName.CompareTo(b2.CustomName);
+      });
+      foreach(IMyFunctionalBlock b in blocks) {
+        MyIniParseResult br;
+        if(_ini.TryParse(b.CustomData, out br) && _ini.Get("switchable", "spinning").ToBoolean()) {
+          menu.AddItem(new BlockSwitcher(b, _bar));
+        } else {
+          menu.AddItem(new BlockSwitcher(b));
+        }
+      }
+      return menu;
     }
 
     public void Main(string argument) {
